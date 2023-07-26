f9980ec7086efa5bc33420b232dfac4744429659
gitoff-start <replyban@gmail.com>
2023-07-26 10:10:43 +0500

    Add project files.

---
+++ dotnetapp/Data/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using dotnetapp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnetapp.Data
+{
+    public class ApplicationDBContext: DbContext
+
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) :base (options)
+        {
+                
+        }
+
+        public DbSet<Category> categories { get; set; } //its create categories table in DB and app connect through this
+
+    }
+}
---
+++ dotnetapp/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dotnetapp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+
+        [Required]
+        public int DisplayOrder { get; set; }
+
+
+
+    }
+}
